40dbafbd96a13eb5d56f9215c50bfee754411b05
dishadhruva <dishapoornima2005@gmail.com>
2025-02-19 21:37:35 +0530

    Create pgm3.c

---
+++ day3ass/pgm3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, scores[100], highest, lowest, sum = 0;
+    float average;
+    
+    printf("Enter number of matches: ");
+    scanf("%d", &n);
+    
+    printf("Enter scores: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &scores[i]);
+        sum += scores[i];
+        if (i == 0) {
+            highest = lowest = scores[i];
+        } else {
+            if (scores[i] > highest) highest = scores[i];
+            if (scores[i] < lowest) lowest = scores[i];
+        }
+    }
+    
+    average = (float)sum / n;
+    
+    printf("Highest Score: %d\n", highest);
+    printf("Lowest Score: %d\n", lowest);
+    printf("Average Score: %.2f\n", average);
+    
+    return 0;
+}
